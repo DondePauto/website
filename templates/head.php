68c567cbef380024baa6a599b28d7017195b1187
Andrés Felipe Torres <af.torres254@gmail.com>
2017-09-23 18:17:40 -0500

    Actualización de recursos del tema
    - Añadir soporte a favicons
    - Modificación de estilos de Bootstrap
    - Modificación de las plantillas básica y de página

--- templates/head.php
+++ templates/head.php
@@ -1,6 +1,20 @@
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <?php wp_head(); ?>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!-- FAVICONS -->
+	<?php $DIR = get_stylesheet_directory_uri()."/assets/favicons"; ?>
+	<link rel="apple-touch-icon" sizes="180x180" href="<?= $DIR ?>/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?= $DIR ?>/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?= $DIR ?>/favicon-16x16.png">
+	<link rel="manifest" href="<?= $DIR ?>/manifest.json">
+	<link rel="mask-icon" href="<?= $DIR ?>/safari-pinned-tab.svg" color="#5bbad5">
+	<link rel="shortcut icon" href="<?= $DIR ?>/favicon.ico">
+	<meta name="apple-mobile-web-app-title" content="DondePauto.CO">
+	<meta name="application-name" content="DondePauto.CO">
+	<meta name="msapplication-config" content="<?= $DIR ?>/browserconfig.xml">
+	<meta name="theme-color" content="#ffffff">
+	
+	<?php wp_head(); ?>
 </head>
