3e24c6ceeea01e741208ffa75df1b2ceaa3405ba
Andrés Felipe Torres <af.torres254@gmail.com>
2018-02-10 18:53:20 -0500

    Actualizar archivos de favicons

--- lib/extras/add_admin_favicons.php
+++ lib/extras/add_admin_favicons.php
@@ -1,8 +1,15 @@
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
 
+namespace Roots\Sage\Extras;
+
+/**
+ * Add favicons for the WP Admin site
+ *
+ * @return null
+ */
+add_action('admin_head', __NAMESPACE__ . '\\add_admin_favicons');
+add_action('login_head', __NAMESPACE__ . '\\add_admin_favicons');
+function add_admin_favicons() { ?>
     <!-- FAVICONS -->
     <link rel="apple-touch-icon" sizes="180x180" href="https://files.dondepauto.co/favicons/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="https://files.dondepauto.co/favicons/favicon-32x32.png">
@@ -14,6 +21,4 @@
     <meta name="application-name" content="{{ config('dondepauto.name.long') }}">
     <meta name="msapplication-config" content="https://files.dondepauto.co/favicons/browserconfig.xml">
     <meta name="theme-color" content="#ffffff">
-
-    <?php wp_head(); ?>
-</head>
+<?php } ?>
