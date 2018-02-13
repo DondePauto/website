c1a3a8402259782d24d47b1b34703476f7d237ed
Andrés Felipe Torres <af.torres254@gmail.com>
2018-02-12 23:43:08 -0500

    Actualizar favicons del sitio

--- templates/head.php
+++ templates/head.php
@@ -7,11 +7,13 @@
     <link rel="apple-touch-icon" sizes="180x180" href="https://files.dondepauto.co/favicons/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="https://files.dondepauto.co/favicons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="https://files.dondepauto.co/favicons/favicon-16x16.png">
-    <link rel="manifest" href="https://files.dondepauto.co/favicons/manifest.json">
-    <link rel="mask-icon" href="https://files.dondepauto.co/favicons/safari-pinned-tab.svg') }}" color="#5">
+    <link rel="manifest" href="https://files.dondepauto.co/favicons/site.webmanifest">
+    <link rel="mask-icon" href="https://files.dondepauto.co/favicons/safari-pinned-tab.svg" color="#00aeef">
     <link rel="shortcut icon" href="https://files.dondepauto.co/favicons/favicon.ico">
-    <meta name="apple-mobile-web-app-title" content="{{ config('dondepauto.name.long') }}">
-    <meta name="application-name" content="{{ config('dondepauto.name.long') }}">
+    <meta name="apple-mobile-web-app-title" content="D&oacute;ndePauto.CO">
+    <meta name="application-name" content="D&oacute;ndePauto.CO">
+    <meta name="msapplication-TileColor" content="#00aeef">
+    <meta name="msapplication-TileImage" content="https://files.dondepauto.co/favicons/mstile-144x144.png">
     <meta name="msapplication-config" content="https://files.dondepauto.co/favicons/browserconfig.xml">
     <meta name="theme-color" content="#ffffff">
 
