9cd959d196ae5d6d2439c1e682cd920c67985119
Andrés Felipe Torres <af.torres254@gmail.com>
2018-04-12 01:21:11 -0500

    Agregar sección de Espacios destacados

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -40,10 +40,53 @@
             <a href="#" id="btn-medios">¿Quieres ofertar tus espacios en DondePauto?</a>
         </div>
     </div>
+    <div class="row text-center" id="espacios-destacados">
+        <div class="full-width">
+            <img src="images/home/icon-destacados.png">
+            <h2>Los espacios más destacados</h2>
+            <div class="row">
+                @foreach( DondePauto\Models\Espacio::destacados() as $espacio )
+                    <div class="col-6 col-sm-3">
+                        <div class="card espacio">
+                            <div class="card-img-top" style="background-image:url(//files.dondepauto.co/{{ $espacio->miniatura }});"></div>
+                            <div class="card-body text-center">
+                                <div class="card-title">{{ $espacio->titulo }}</div>
+                                <hr class="card-separator">
+                                <div class="categoria">{{ $espacio->data->categoria->nombre }}</div>
+                                <a href="/espacios/{{ $espacio->slug }}" type="button" class="btn btn-sm btn-danger">
+                                    <b>Ver más</b>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+            <a href="/buscar" type="button" class="btn btn-lg btn-danger" id="btn-buscar-espacios">
+                <span>Conoce nuestros espacios</span> <i class="fa fa-fw fa-chevron-right"></i>
+            </a><br>
+        </div>
+    </div>
+@endsection
+
+@section('javascript')
+<script type="text/javascript">
+    /* global $ */
+    document.addEventListener('DOMContentLoaded', function(event) {
+        $(function() {
+            $(window).resize(function() {
+                $('.espacio .card-img-top').css('minHeight', $('.espacio .card-img-top').width());
+                $('.espacio .card-img-top').css('maxHeight', $('.espacio .card-img-top').width());
+            }).resize();
+        });
+    });
+</script>
 @endsection
 
 @section('css')
 <style type="text/css">
+    a[type=button] {
+        -webkit-appearance: none;
+    }
     [id^=banner-] {
         height: calc(100vh - 45px);
         background-color: lightgray;
@@ -67,6 +110,10 @@
         line-height: 1.5rem;
         text-transform: none;
     }
+    h2 {
+        font-size: 1.75rem;
+        font-weight: bold;
+    }
     #banner-buscar,
     #banner-registrar {
         color: white;
@@ -84,30 +131,91 @@
     #banner-buscar #form {
         margin-top: 45px;
     }
-    #banner-info img {
+    #banner-info img,
+    #espacios-destacados .full-width>img {
         max-width: 100%;
         margin-bottom: 15px;
     }
     #banner-info h2 {
-        font-size: 1.75rem;
-        font-weight: bold;
         color: {{ config('dondepauto.colores.lightblue') }};
     }
     #banner-registrar .heading {
         text-transform: none;
     }
-    #banner-registrar #btn-registrar {
-        margin: 50px auto;
-        font-size: 25px;
+    #banner-registrar #btn-registrar,
+    #espacios-destacados #btn-buscar-espacios {
+        margin: 20px auto;
+        font-size: 16px;
         font-weight: bold;
         color: white;
-        -webkit-appearance: none;
     }
     #banner-registrar #btn-medios {
         padding: 2.5px;
         color: white;
         text-decoration: underline;
         display: inline-block;
+    }
+    #espacios-destacados .full-width {
+        padding: 15px;
+    }
+    #espacios-destacados h2 {
+        margin-bottom: 15px;
+    }
+    #espacios-destacados .row {
+        margin-right: -7.5px;
+        margin-left: -7.5px;
+    }
+    #espacios-destacados .row>div {
+        padding: 0 7.5px;
+    }
+    #espacios-destacados .espacio {
+        min-height: 260px;
+        margin-bottom: 15px;
+        padding: 5px;
+        background-color: lightgray;
+    }
+    #espacios-destacados .espacio .card-img-top {
+        background-color: lightgray;
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+    }
+    #espacios-destacados .espacio .card-body {
+        padding: 5px 0;
+        position: relative;
+    }
+    #espacios-destacados .espacio .card-title {
+        height: 49px;
+        max-height: 49px;
+        margin: 5px 5px 0;
+        font-size: 16px;
+        font-weight: bold;
+        line-height: 1;
+        text-overflow: ellipsis;
+        display: block;
+        display: -webkit-box;
+        overflow: hidden;
+        -webkit-line-clamp: 3;
+        -webkit-box-orient: vertical;
+    }
+    #espacios-destacados .espacio .card-separator {
+        width: 80%;
+        margin: 10px 10%;
+    }
+    #espacios-destacados .espacio .categoria {
+        height: 29px;
+        max-height: 29px;
+        font-size: 14px;
+        line-height: 1;
+        text-overflow: ellipsis;
+        display: block;
+        display: -webkit-box;
+        overflow: hidden;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+    }
+    #espacios-destacados .espacio a {
+        margin-top: 15px;
     }
     @media(max-width:576px) {
         #banner-buscar {
@@ -133,16 +241,25 @@
             font-size: 2.25rem;
             line-height: 2.25rem;
         }
+        h2 {
+            margin-bottom: 30px;
+            font-size: 2.75rem;
+        }
         #banner-buscar #form {
             width: 75%;
             margin: 45px 12.5% 0;
         }
-        #banner-info h2 {
-            margin-bottom: 30px;
-            font-size: 2.75rem;
-        }
         #banner-info p {
             font-size: 1.5rem;
+        }
+        #banner-registrar #btn-registrar,
+        #espacios-destacados #btn-buscar-espacios {
+            margin: 50px auto;
+            font-size: 25px;
+        }
+        #espacios-destacados .row {
+            margin-right: calc(5% - 7.5px);
+            margin-left: calc(5% - 7.5px);
         }
     }
 </style>
