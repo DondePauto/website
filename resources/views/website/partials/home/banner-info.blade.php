ad50b666550b79e6e92cc22b8ac1983c74420924
Andrés Felipe Torres <af.torres254@gmail.com>
2018-04-15 20:31:43 -0500

    Actualizar a número de espacios dinámico

--- resources/views/website/partials/home/banner-info.blade.php
+++ resources/views/website/partials/home/banner-info.blade.php
@@ -3,7 +3,7 @@
         <img src="images/home/icon-info.png" class="header-icon">
         <h2 class="h2">Haz crecer tu negocio con publicidad efectiva</h2>
         <p>En DóndePauto encuentras los medios publicitarios más efectivos para tu negocio y asesoría especializada para que llegues a tus verdaderos clientes.</p>
-        <p>Contamos con más de 800 espacios de pauta en todo Colombia para que puedas dar a conocer tu empresa y aumentar la confianza de los clientes que ya te conocen.</p>
+        <p>Contamos con más de {{ DondePauto\Models\Espacio::total() }} espacios de pauta en todo Colombia para que puedas dar a conocer tu empresa y aumentar la confianza de los clientes que ya te conocen.</p>
     </div>
 
     <style type="text/css">
