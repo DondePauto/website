2c48ca85d2647ad11c2f7c8ec2a0e672d868551d
Andrés Felipe Torres <af.torres254@gmail.com>
2018-04-22 11:13:17 -0500

    Actualización de archivos de Yarn

--- resources/assets/build/webpack.config.optimize.js
+++ resources/assets/build/webpack.config.optimize.js
@@ -6,14 +6,14 @@ const imageminMozjpeg = require('imagemin-mozjpeg');
 const config = require('./config');
 
 module.exports = {
-  plugins: [
-    new ImageminPlugin({
-      optipng: { optimizationLevel: 7 },
-      gifsicle: { optimizationLevel: 3 },
-      pngquant: { quality: '65-90', speed: 4 },
-      svgo: { removeUnknownsAndDefaults: false, cleanupIDs: false },
-      plugins: [imageminMozjpeg({ quality: 75 })],
-      disable: (config.enabled.watcher),
-    }),
-  ],
+    plugins: [
+        new ImageminPlugin({
+            optipng: { optimizationLevel: 7 },
+            gifsicle: { optimizationLevel: 3 },
+            pngquant: { quality: '65-90', speed: 4 },
+            svgo: { removeUnknownsAndDefaults: false, cleanupIDs: false },
+            plugins: [imageminMozjpeg({ quality: 75 })],
+            disable: (config.enabled.watcher),
+        }),
+    ],
 };
