70cff4f44ab6e288891176e70f6546141539e27f
Andrés Felipe Torres <af.torres254@gmail.com>
2018-05-14 16:55:11 -0500

    Actualizar descripciones de páginas

--- resources/views/website/partials/seo.blade.php
+++ resources/views/website/partials/seo.blade.php
@@ -1,6 +1,7 @@
 <!-- META TAGS -->
-<title>@yield('title', config('app.name').' - '.setting('website.descripcion'))</title>
-<meta name="description" content="@yield('description', setting('website.descripcion'))">
+<?php define('RUTA', request()->route() ? request()->route()->getName() : 'home'); ?>
+<title>@yield('title', config('app.name').' - '.setting('website.descripcion_'.RUTA))</title>
+<meta name="description" content="@yield('description', setting('website.descripcion_'.RUTA))">
 <meta name="copyright" content="{{ config('app.name') }}">
 <meta name="author" content="{{ config('app.name') }}">
 <meta name="application-name" content="{{ config('app.name') }}">
@@ -9,14 +10,14 @@
 <meta property="og:site_name" content="{{ config('app.name') }}">
 <meta property="og:type" content="website">
 <meta property="og:url" content="{{ request()->fullUrl() }}">
-<meta property="og:title" content="@yield('title', config('app.name').' - '.setting('website.descripcion'))">
-<meta property="og:description" content="@yield('description', setting('website.descripcion'))">
-<meta property="og:image" content="@yield('image', '//files.dondepauto.co/favicons/android-chrome-192x192.png')">
+<meta property="og:title" content="@yield('title', config('app.name').' - '.setting('website.descripcion_'.RUTA))">
+<meta property="og:description" content="@yield('description', setting('website.descripcion_'.RUTA))">
+<meta property="og:image" content="@yield('image', route('files.favicons', ['path' => 'android-chrome-192x192.png']))">
 <meta property="og:locale" content="es_CO">
 
 <!-- TWITTER TAGS -->
-<meta name="twitter:card" content="@yield('description', setting('website.descripcion'))">
+<meta name="twitter:card" content="@yield('description', setting('website.descripcion_'.RUTA))">
 <meta name="twitter:site" content="{{ '@'.json_decode(setting('contacto.social'))->twitter }}">
-<meta name="twitter:title" content="@yield('title', config('app.name').' - '.setting('website.descripcion'))">
-<meta name="twitter:description" content="@yield('description', setting('website.descripcion'))">
-<meta name="twitter:image" content="@yield('image', '//files.dondepauto.co/favicons/android-chrome-192x192.png')">
+<meta name="twitter:title" content="@yield('title', config('app.name').' - '.setting('website.descripcion_'.RUTA))">
+<meta name="twitter:description" content="@yield('description', setting('website.descripcion_'.RUTA))">
+<meta name="twitter:image" content="@yield('image', route('files.favicons', ['path' => 'android-chrome-192x192.png']))">
